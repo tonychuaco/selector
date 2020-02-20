df908c6c6db5a85117214fbc7aa21978f67e349c
unknown <16738612+tonychuaco@users.noreply.github.com>
2020-02-19 21:56:40 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import Shape from './shape';
+
+class Selector extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      selectedShape: "square",
+    }
+  }
+
+  // this is where our methods live
+
+  selectShape = (shapeName) => {
+    this.setState({
+      selectedShape: shapeName,
+    })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="navbar">
+          <div>Selected: <span>{this.state.selectedShape}</span></div>
+        </div>
+        <div className="shape-list">
+          <Shape shape="square" selectShape={this.selectShape} />
+          <Shape shape="circle" selectShape={this.selectShape} />
+          <Shape shape="triangle" selectShape={this.selectShape} />
+        </div>
+      </div>
+    )
+  }
+
+}
+
+export default Selector;
---
+++ src/shape.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Shape = (props) => {
+const shape = props.shape;
+const selectShape = props.selectShape;
+
+  return (
+    <div className={shape} onClick={() => selectShape(shape)} />
+  )
+};
+
+export default Shape;
